866033e3de2f033591e08c87b2fd9f9f65589a63
Sayak Kundu <xs242-saykun@xs-host603-010.xs.neuralcompany.team>
2022-07-28 14:45:52 +0530

    two java files added

---
+++ HelloWorld.java
@@ -0,0 +1,9 @@
+class HelloWorld {
+
+    public static void main(String[] args) {
+        
+        System.out.println("Hello Java from Jenkins!");
+        // home/xs242-saykun/Desktop/JenkinsJava/HelloWorld.java
+
+    }
+}
